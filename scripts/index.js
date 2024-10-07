e74e95b86635cc56c9b104d5660e038434389b67
Paul Glatt <paul.glatt57@gmail.com>
2024-10-07 19:11:47 +0200

    améliorations

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,10 @@ import { recipes } from "../data/recipes.js";
 import { createCardsDOM } from "./template.js";
 import { remplirDropdowns } from "./dropdown.js";
 
-// Afficher le nombre de recettes sur la page
+const container = document.getElementById("recipes-container");
+const cardsElement = createCardsDOM(recipes);
 
-// Afficher les recettes sur la page
 function init() {
-    const container = document.getElementById("recipes-container");
-    const cardsElement = createCardsDOM(recipes);
     container.append(...cardsElement);
     remplirDropdowns();
 }
